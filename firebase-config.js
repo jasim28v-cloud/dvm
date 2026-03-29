2d72e7e9a461357851adca0cb0ad56030663269d
jasim28v-cloud <jasim28v@gmail.com>
2026-03-29 12:40:25 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,28 +1,19 @@
-// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
-  authDomain: "tope-e5350.firebaseapp.com",
-  projectId: "tope-e5350",
-  storageBucket: "tope-e5350.firebasestorage.app",
-  messagingSenderId: "187788115549",
-  appId: "1:187788115549:web:5012a1053d2ff7dced97b4",
-  measurementId: "G-V1XM95PMQC"
+    apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
+    authDomain: "tope-e5350.firebaseapp.com",
+    databaseURL: "https://fokx-c135a-default-rtdb.firebaseio.com",
+    projectId: "tope-e5350",
+    storageBucket: "tope-e5350.firebasestorage.app",
+    messagingSenderId: "187788115549",
+    appId: "1:187788115549:web:5012a1053d2ff7dced97b4"
 };
 
-// بما أن الكود الأصلي يستخدم Realtime Database، يجب إضافة databaseURL
-// لأن الإعدادات الجديدة لا تحتوي عليه، لكن المستخدم قد قام بتوفير رابط الـ RTDB:
-// https://tope-e5350-default-rtdb.firebaseio.com/
-// لذا سأضيفه يدوياً
-firebaseConfig.databaseURL = "https://tope-e5350-default-rtdb.firebaseio.com";
-
-// تهيئة Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
 
-// إعدادات Cloudinary الجديدة
 const CLOUD_NAME = 'dnmpmysk6';
 const UPLOAD_PRESET = 'rsxdfdgw';
 
-console.log('✅ SHΔDØW System Ready (Updated Firebase + Cloudinary)');
+console.log('✅ SHΔDØW System Ready');
